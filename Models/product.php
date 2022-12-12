b1ec5e7e46d8228dddb01228fb3e677d71139148
SaverioCascelli <saverio.cascelli@gmail.com>
2022-12-12 15:57:27 +0100

    [add] product class

--- Models/product.php
+++ Models/product.php
@@ -5,10 +5,27 @@ class product
     public $name;
     public $brand;
     public $price;
-    public $is_avaiable = false;
+    private $is_avaiable = false;
     public $img_src;
 
-    public function __construct($_id, $_name, $_brand, $_price, $img_src = null)
+    /**
+     * @var int id
+     * @var string $name
+     * @var string brand
+     * @var float price
+     * @var string img_src placeholder if null
+     */
+    public function __construct($_id, $_name, $_brand, $_price, $_img_src = null)
     {
+        $this->id = $_id;
+        $this->name = $_name;
+        $this->brand = $_brand;
+        $this->price = $_price;
+        $this->img_src = $_img_src;
+    }
+
+    public function toggleAvaiable()
+    {
+        $this->is_avaiable = !($this->is_avaiable);
     }
 }
